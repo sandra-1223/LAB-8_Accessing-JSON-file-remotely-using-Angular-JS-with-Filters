efd9f224299fa97f94255b1e782945de404e0d3e
Sandra Sony <89525911+sandra-1223@users.noreply.github.com>
2022-02-03 08:52:24 +0530

    Add files via upload

---
+++ labemp.js
@@ -0,0 +1,9 @@
+var sample=angular.module("sample", []);
+sample.controller("myctrl", function($scope, $http)
+{
+    $http.get('data.json')
+    .success(function(response)
+    {
+        $scope.entry=response.data;
+    });
+});
