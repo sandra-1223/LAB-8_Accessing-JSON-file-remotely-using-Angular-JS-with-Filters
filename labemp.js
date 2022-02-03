c21546ae0ea299de1fc4e6e88291cf9e34c82658
Sandra Sony <89525911+sandra-1223@users.noreply.github.com>
2022-02-03 15:06:30 +0530

    Update labemp.js

--- labemp.js
+++ labemp.js
@@ -1,7 +1,7 @@
 var sample=angular.module("sample", []);
 sample.controller("myctrl", function($scope, $http)
 {
-    $http.get('data.json')
+    $http.get('https://sandra-1223.github.io/LAB-8_Accessing-JSON-file-remotely-using-Angular-JS-with-Filters/data.json')
     .success(function(response)
     {
         $scope.entry=response.data;
